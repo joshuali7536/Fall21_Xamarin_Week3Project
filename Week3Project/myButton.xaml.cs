998ae99abf0c8bd6e8a74650aff7ef82c125e69b
rania <raniaarbash@hotmail.com>
2021-09-27 17:59:03 -0400

    * AddNewContactPage.xaml: Week4 Project
    * dateCell.xaml:
    * myButton.xaml:
    * dateCell.xaml.cs:
    * myButton.xaml.cs:
    * Contact.cs:
    * AddNewContactPage.xaml.cs:

---
+++ Week3Project/myButton.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace Week3Project
+{
+    public partial class myButton : Button
+    {
+        public myButton()
+        {
+            InitializeComponent();
+        }
+    }
+}
